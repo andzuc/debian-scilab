4e501bfae1f790b71439d6e4f12eef363f0ed61c
Andrea Zuccherelli <andrea@zuccherelli.net>
2020-04-03 17:11:14 +0200

    working ok

--- runbash.sh
+++ runbash.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-docker run --rm -it \
+#export LIBGL_ALWAYS_INDIRECT=0
+export LIBGL_ALWAYS_SOFTWARE=1
+export MESA_GL_VERSION_OVERRIDE=3.0
+PROFILEDIR="${HOME}/dev/scilab"
+
+docker run --rm -it --net=host\
        --env="DISPLAY" \
-       -v "${HOME}/.Xauthority:/home/scilab/.Xauthority:rw" \
+       --env="LIBGL_ALWAYS_INDIRECT" \
+       --env="LIBGL_ALWAYS_SOFTWARE" \
+       --env="MESA_GL_VERSION_OVERRIDE" \
+       --mount "type=bind,src=${HOME}/.Xauthority,dst=/home/scilab/.Xauthority,readonly" \
+       --mount "type=bind,src=${PROFILEDIR},dst=/home/scilab/.Scilab" \
        --entrypoint /bin/bash \
        andzuc/debian-scilab
