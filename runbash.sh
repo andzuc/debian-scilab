0673e541ee7201628a289ec288acb6a19833bab6
Andrea Zuccherelli <andrea@zuccherelli.net>
2020-04-18 18:48:58 +0200

    test 6.1.0

--- runbash.sh
+++ runbash.sh
@@ -12,4 +12,4 @@ docker run --rm -it --net=host\
        --mount "type=bind,src=${HOME}/.Xauthority,dst=/home/scilab/.Xauthority,readonly" \
        --mount "type=bind,src=${PROFILEDIR},dst=/home/scilab" \
        --entrypoint /bin/bash \
-       andzuc/debian-scilab:2020041704
+       andzuc/debian-scilab:2020041800
